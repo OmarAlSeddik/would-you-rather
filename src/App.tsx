731a5ddc223bf2ffb60369e9ecfc0d73bdad9bc6
Omar <omaralseddik@gmail.com>
2021-11-08 17:59:18 +0200

    Initialized the Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import { ThemeContextProvider } from "./context/ThemeContext";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContextProvider>
+      <HashRouter>
+        <Header />
+        <Routes>
+          <Route />
+          <Route />
+        </Routes>
+      </HashRouter>
+    </ThemeContextProvider>
   );
-}
+};
 
 export default App;
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useMemo } from "react";
+import {
+  createTheme,
+  alpha,
+  responsiveFontSizes,
+  useMediaQuery,
+  ThemeProvider,
+  CssBaseline,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
+
+const ThemeContext = createContext({
+  theme: {},
+  isMobile: false,
+});
+
+export const ThemeContextProvider = (props: any) => {
+  let theme: any = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          primary: {
+            main: blue[900],
+          },
+          background: {
+            default: alpha(blue[900], 0.1),
+            paper: alpha("#fff", 0.9),
+          },
+        },
+      }),
+    []
+  );
+
+  theme = responsiveFontSizes(theme);
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <ThemeContext.Provider value={{ theme, isMobile }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {props.children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
